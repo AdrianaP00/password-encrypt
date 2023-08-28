769f9f7b763d99e6f927f471b79659a12359f85d
Adriana Piccolo <adrianapiccolo00@gmail.com>
2023-08-29 00:06:24 +0200

    auth

--- src/api/routes/AuthRoutes.ts
+++ src/api/routes/AuthRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import {register, login } from "../controller/AuthCtrl";
-const {isAuth, isAdmin} = require("../../middlewares/auth")
+import { isAdmin, isAuth } from "../../middlewere/auth";
 
 const authRoutes = express.Router();
 
--- src/api/routes/UserRoutes.ts
+++ src/api/routes/UserRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { getUsers, deleteUser , putUser , postUser } from "../controller/UserCtrl";
-const {isAuth, isAdmin} = require("../../middlewares/auth")
+import { isAdmin, isAuth } from "../../middlewere/auth";
 
 const userRouter = express.Router();
 
-userRouter.get("/", getUsers);
+userRouter.get("/",getUsers);
 userRouter.post("/", postUser);
 userRouter.put("/:id", putUser);
 userRouter.delete("/:id", deleteUser);
